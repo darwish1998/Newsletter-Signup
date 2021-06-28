a08a53d3079ee0e39a7b11c2d9ccb9d36057821a
Ahmed <darwish144.ad@gmail.com>
2021-06-28 21:59:29 +0300

    1 project

---
+++ app.js
@@ -0,0 +1,15 @@
+//jshint esversion:6
+
+const express = require(" express ");
+const bodyPaser = require("body-parse");
+const request = require("request");
+
+
+const app = express();
+
+app.listen(2000, function () {
+
+    console.log("Sserver is running at port 2000");
+
+
+});
